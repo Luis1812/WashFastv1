a0f928f8fa899228a5f0f514111934270e5c5bb4
Luis Castillo <luis.castillo.cuyutupa@gmail.com>
2021-04-29 13:52:33 -0500

    Update Usuario.java

--- src/main/java/pe/edu/upc/entity/Usuario.java
+++ src/main/java/pe/edu/upc/entity/Usuario.java
@@ -13,9 +13,15 @@ public class Usuario implements Serializable{
 	@Id
     private int idUsuario;
 
+	private String usuario;
+	
+	private String contrasena;
+	
+	private char sexo;
+	
     private String direccion;
 
-    private int telefono;
+    private String telefono;
 
     private String nombre;
 
@@ -23,43 +29,95 @@ public class Usuario implements Serializable{
 
     private String correo;
 
-    public String getNombre() {
-        return nombre;
-    }
+	public Usuario(int idUsuario, String usuario, String contrasena, char sexo, String direccion, String telefono,
+			String nombre, String apellido, String correo) {
+		super();
+		this.idUsuario = idUsuario;
+		this.usuario = usuario;
+		this.contrasena = contrasena;
+		this.sexo = sexo;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.correo = correo;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-    public String getDireccion() {
-        return direccion;
-    }
+	public int getIdUsuario() {
+		return idUsuario;
+	}
 
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
 
-    public int getTelefono() {
-        return telefono;
-    }
+	public String getUsuario() {
+		return usuario;
+	}
 
-    public void setTelefono(int telefono) {
-        this.telefono = telefono;
-    }
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
 
-    public String getApellido() {
-        return apellido;
-    }
+	public String getContrasena() {
+		return contrasena;
+	}
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
 
-    public String getCorreo() {
-        return correo;
-    }
+	public char getSexo() {
+		return sexo;
+	}
 
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
 }
