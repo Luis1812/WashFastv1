ee02d07c7a7fecd93a60fc6a09829349fadfda78
Luis Castillo <luis.castillo.cuyutupa@gmail.com>
2021-04-29 09:38:48 -0500

    Creación de entidad Local
    Se creó entidad Local pero falta agregar atributos

---
+++ src/main/java/pe/edu/upc/entity/Local.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Local implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+}
